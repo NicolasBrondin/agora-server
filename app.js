30bea20eb81708333fb08544e6f81d444306d45a
NicolasBrondin <nicolas@brondin.com>
2017-02-01 07:46:17 +0100

    [REFACTOR] Put rooms location in their own file

--- app.js
+++ app.js
@@ -3,34 +3,24 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var cors = require('cors');
 
+var rooms = require('./data/geo.json');
 
-
-var rooms = [
-    {name:"World", level: 0},
-    {name:"France", level:1, position: {lat:47.052705, long: 2.766922, radius: 300000 } },  
-    {name:"Marçay", level:2, position: {lat:46.470203, long: 0.226018, radius: 4000 } },
-    {name:"Limoges", level:2, position: {lat:45.854794, long: 1.247002, radius: 6000 } } //, 
-];
-
-
-  Number.prototype.toRad = function() {
+Number.prototype.toRad = function() {
     return this * Math.PI / 180;
-  }
+}
 
 
 function calculate_distance(long1,lat1,long2 , lat2) {
     var R = 6371; // Radius of the earth in km
-  var dLat = (lat2-lat1).toRad();  // Javascript functions in radians
-  var dLon = (long2-long1).toRad(); 
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-          Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
-          Math.sin(dLon/2) * Math.sin(dLon/2); 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c * 1000; // Distance in m
-  return d;
+    var dLat = (lat2-lat1).toRad();  // Javascript functions in radians
+    var dLon = (long2-long1).toRad(); 
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * Math.sin(dLon/2) * Math.sin(dLon/2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c * 1000; // Distance in m
+    return d;
 } // CalculateDistance
 
-app.use(cors())
+app.use(cors());
 
 app.get('/rooms/lat/:lat/long/:long', function(req, res){
     var available = rooms.filter(function(room){
@@ -45,13 +35,10 @@ app.get('/rooms/lat/:lat/long/:long', function(req, res){
 
 io.on('connection', function(socket){
     
-  var defaultRoom = 'general';
-  var rooms = ["General", "angular", "socket.io", "express", "node", "mongo", "PHP", "laravel"];
+  var default_room = 'World';
 
   //Emit the rooms array
-  socket.emit('setup', {
-    rooms: rooms
-  });
+  socket.emit('setup');
     
   //Listens for new user
   socket.on('setup', function(data) {
@@ -65,8 +52,9 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on *:3000');
+var port = process.env.PORT || 3000;
+http.listen(port, function(){
+  console.log('listening on port: '+ port);
 });
 
 /*
